fb14f7a40899de49c53bff5552f0e5c9040f6d8f
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-04-07 10:38:36 -0700

    Add display for all members in database with styling

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Router } from '@angular/router';
+import { Member } from '../member.model';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  styleUrls: ['./member-list.component.css'],
+  providers: [MemberService]
 })
 export class MemberListComponent implements OnInit {
+  members;
 
-  constructor() { }
+  constructor(private router: Router, private memberService: MemberService) { }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers()
   }
 
 }
